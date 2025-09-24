a9ae375e62536ecf3102408a6c612248519c02aa
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-09-24 22:36:25 +0530

    made changes

--- frontend/src/utils/uploadImage.js
+++ frontend/src/utils/uploadImage.js
@@ -1,22 +1,22 @@
 import { API_PATHS } from "./apiPaths";
 import axiosinstance from "./axiosInstance";
 
-const uploadImage=async(imageFile)=>{
-    const formdata=new FormData();
-    formdata.append('image',imageFile)
+const uploadImage = async (imageFile) => {
+  const formdata = new FormData();
+  formdata.append("image", imageFile);
 
-    try{
-        const res=await axiosinstance.post(API_PATHS.IMAGE.UPLOAD_IMAGE,formdata,{
-            headers:{
-                "Content-Type":"multipart/form-data"
-            }
-        })
-        return res.data.imageurl;
+  try {
+    const res = await axiosinstance.post(
+      "https://expense-tracker-backend-ml9d.onrender.com/api/v1/auth/upload-image",
+      formdata,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
 
-    }catch(err){
-        console.error("Error uploading the image:",err)
-        throw err;
-    }
-}
+    return res.data.imageurl;
+  } catch (err) {
+    console.error("Error uploading the image:", err);
+    throw err;
+  }
+};
 
-export default uploadImage
+export default uploadImage;
